874e0a4dede3ef697bd3593563fb5aa5385005f2
jonathan.sarion <jonathan.sarion@pointwest.com.ph>
2019-05-06 18:55:15 +0800

    initial commit

---
+++ SampleGit/src/main/java/com/samplegit/controller/AuthenticationController.java
@@ -0,0 +1,24 @@
+package com.samplegit.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+
+	/*@Autowired
+	EmployeeManager  employeeManager;
+	
+	@RequestMapping(value="/test", method=RequestMethod.GET)
+	public String test(){
+		return "Hello Test";
+	}
+	
+	@RequestMapping(value="/test/employee", method=RequestMethod.GET)
+	public Employee testEmployee(){
+		Employee emp;
+		
+		emp = employeeManager.testEmployee();
+		System.out.println(emp);
+	return emp;
+	}*/
+}
