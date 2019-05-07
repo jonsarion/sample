d7b4d323c9ff9f0a54488d1817d5fc9f39d8c53b
jonathan.sarion <jonathan.sarion@pointwest.com.ph>
2019-05-07 14:27:15 +0800

    added sample text

--- SampleGit/src/main/java/com/samplegit/controller/AuthenticationController.java
+++ SampleGit/src/main/java/com/samplegit/controller/AuthenticationController.java
@@ -24,7 +24,7 @@ public class AuthenticationController {
 		Employee emp;
 		
 		emp = employeeManager.testEmployee();
-		System.out.println(emp);
+		System.out.println(emp + " hello");
 	return emp;
 	}
 }
